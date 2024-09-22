1618f73fc7fb64142b036a84fe0dccf2ea4b197e
unknown <aroragarima129@gmail.com>
2024-09-23 00:00:24 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,24 +18,35 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
             model = list(models.values())[i]
+            para = param[model_name]
 
+            # Perform Grid Search with the model and its parameters
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            # Set the model with the best parameters
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
+            # Make predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
+            # Calculate the R2 score
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            # Store the test model score in the report
+            report[model_name] = test_model_score
 
         return report
     
     except Exception as e:
-        return CustomException(e, sys)
+        raise CustomException(e, sys)
